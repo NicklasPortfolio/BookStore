c8777e0336ccaab43cff770767dccdea556bc468
starbucks-employee <68434408+starbucks-employee@users.noreply.github.com>
2023-10-17 21:14:55 +0200

    final update

--- www/update.php
+++ www/update.php
@@ -2,57 +2,53 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once("../config/db_config.php");
 
-    $id = $_POST["id"];
-    $title = $_POST["Title"];
-    $author = $_POST["Author"];
-    $genre = $_POST["Genre"];
-    $publisher = $_POST["Publisher"];
+    $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+    $title = filter_input(INPUT_POST, 'Title', FILTER_SANITIZE_STRING);
+    $author = filter_input(INPUT_POST, 'Author', FILTER_SANITIZE_STRING);
+    $genre = filter_input(INPUT_POST, 'Genre', FILTER_SANITIZE_STRING);
+    $publisher = filter_input(INPUT_POST, 'Publisher', FILTER_SANITIZE_STRING);
 
     $sql = "UPDATE books SET ";
     $placeholders = [];
+    $params = [':id' => $id];
 
     if (!empty($title)) {
         $placeholders[] = "Title = :title";
+        $params[':title'] = $title;
     }
 
     if (!empty($author)) {
         $placeholders[] = "Author = :author";
+        $params[':author'] = $author;
     }
 
     if (!empty($genre)) {
         $placeholders[] = "Genre = :genre";
+        $params[':genre'] = $genre;
     }
 
     if (!empty($publisher)) {
         $placeholders[] = "Publisher = :publisher";
+        $params[':publisher'] = $publisher;
     }
 
-    $sql .= implode(", ", $placeholders) . " WHERE id = :id";
-
-    // Prepare the query
-    $stmt = $db_connection->prepare($sql);
-
-    // Bind parameters
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    if (!empty($title)) {
-        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
-    }
-    if (!empty($author)) {
-        $stmt->bindParam(':author', $author, PDO::PARAM_STR);
-    }
-    if (!empty($genre)) {
-        $stmt->bindParam(':genre', $genre, PDO::PARAM_STR);
-    }
-    if (!empty($publisher)) {
-        $stmt->bindParam(':publisher', $publisher, PDO::PARAM_STR);
-    }
-
-    // Execute the query
-    if ($stmt->execute()) {
-        header("Location: index.php");
-        exit;
+    if (empty($placeholders)) {
+        echo "No fields to update.";
     } else {
-        echo "Error updating record: " . implode(" - ", $stmt->errorInfo());
+        $sql .= implode(", ", $placeholders) . " WHERE id = :id";
+
+        try {
+            $stmt = $db_connection->prepare($sql);
+
+            if ($stmt->execute($params)) {
+                header("Location: index.php");
+                exit;
+            } else {
+                echo "Error updating record: " . implode(" - ", $stmt->errorInfo());
+            }
+        } catch (PDOException $e) {
+            echo "Database Error: " . $e->getMessage();
+        }
     }
 
     $db_connection = null;
